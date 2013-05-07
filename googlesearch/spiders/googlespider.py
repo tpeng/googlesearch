e7cd959634142ea8a02f345e6b1af547c459d7c0
tpeng <pengtaoo@gmail.com>
2013-05-07 15:51:01 +0800

    use google advance search

--- googlesearch/spiders/googlespider.py
+++ googlesearch/spiders/googlespider.py
@@ -6,31 +6,36 @@ from scrapy.spider import BaseSpider
 from scrapy.utils.response import get_base_url
 from googlesearch.items import GoogleSearchItem
 
+_COUNTRIES = {
+    'ie': 'countryIE'
+}
+
 """
 A spider to parse the google search result.
 """
 class GoogleSearchSpider(BaseSpider):
     name = 'googlesearch'
-    kws = ['contact information', 'pizza', 'restaurant', 'house rent', 'hotel', 'contact me']
-    country = 'com'
+    kws = ('contact us',)
+    region = 'ie'
     savedb = True
     download_delay = 5
+    base_url_fmt = 'http://www.google.{region}/search?hl=en&as_q=&as_epq={query}&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr={country}&as_qdr=all&as_sitesearch={region}&as_occt=any&safe=images&tbs=&as_filetype=&as_rights='
 
     def start_requests(self):
         for kw in self.kws:
-            url = self.make_google_search_request(self.country, kw)
+            url = self.make_google_search_request(_COUNTRIES[self.region], kw)
             yield Request(url=url, meta={'kw': kw})
 
-    def make_google_search_request(self, country, keywords):
-        return 'http://www.google.{}/search?{}'.format(country, urlencode({'q': keywords}))
+    def make_google_search_request(self, country, query):
+        return self.base_url_fmt.format(country=country, region=self.region, query='+'.join(query.split()).strip('+'))
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
         for sel in hxs.select('//div[@id="ires"]//li[@class="g"]//h3[@class="r"]'):
             name = u''.join(sel.select(".//text()").extract())
             url = _parse_url(sel.select('.//a/@href').extract()[0])
-            country = _get_country_code(url)
-            if country == self.country and len(url):
+            region = _get_region(url)
+            if region == self.region and len(url):
                 yield Request(url=url, callback=self.parse_item, meta={'name':name,
                                                                        'kw': response.meta['kw']})
 
@@ -47,7 +52,7 @@ class GoogleSearchSpider(BaseSpider):
         yield GoogleSearchItem({'name': name,
                                 'url': url,
                                 'html': html,
-                                'country': self.country,
+                                'country': self.region,
                                 'kw': kw})
 
     def _build_absolute_url(self, response, url):
@@ -65,13 +70,13 @@ def _parse_url(href):
     queries = dict(parse_qsl(urlparse(href).query))
     return queries.get('q', '')
 
-def _get_country_code(url):
+def _get_region(url):
     """
     get country code from the url.
 
-    >>> _get_country_code('http://scrapinghub.ie')
+    >>> _get_region('http://scrapinghub.ie')
     'ie'
-    >>> _get_country_code('http://www.astoncarpets.ie/contact.htm')
+    >>> _get_region('http://www.astoncarpets.ie/contact.htm')
     'ie'
     """
     netloc = urlparse(url)[1]
